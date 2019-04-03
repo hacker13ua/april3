5e7f4637fa0f4149258cf8790b596b157eba4f76
esurovskiy <surovskiy@trafficdna.com>
2019-04-03 18:57:47 +0300

    1

---
+++ src/main/java/org/esurovskiy/april3/Main1.java
@@ -0,0 +1,52 @@
+package org.esurovskiy.april3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * TODO: write javadoc
+ *
+ * @author Evgeniy Surovskiy
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        String s = "(2-3)+[222]+{2*(2-2)}";
+
+        Deque<Character> deque = new LinkedList<Character>();
+        for (final char c : s.toCharArray()) {
+            if (c == '{' || c == '(' || c == '[') {
+                deque.addLast(c);
+            } else if (c == '}' || c == ')' || c == ']') {
+                final Character open = deque.pollLast();
+                if (isClosed(open, c)) {
+                    continue;
+                }
+                else {
+                    System.out.println("Invalid string");
+                    return;
+                }
+            }
+        }
+        if (deque.size()==0){
+            System.out.println("OK");
+        }
+        else {
+            System.out.println("Invalid");
+        }
+    }
+
+    private static boolean isClosed(final Character open, final Character closed) {
+        if(open.equals('{')){
+            return closed.equals('}');
+        }
+        if(open.equals('(')){
+            return closed.equals(')');
+        }
+        if(open.equals('[')){
+            return closed.equals(']');
+        }
+        return false;
+    }
+}
